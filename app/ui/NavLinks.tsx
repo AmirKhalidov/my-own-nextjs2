3a77bb05afb9d99a411f7e254fac43d983c640da
Amir <amir.khalidov@gmail.com>
2024-09-18 22:09:06 +0400

    initial commit

---
+++ app/ui/NavLinks.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+
+export default function NavLinks() {
+    const pathname = usePathname();
+
+    const links = [
+        {
+            name: 'Homepage',
+            href: '/',
+        },
+        {
+            name: 'Dashboard',
+            href: '/dashboard',
+        },
+        {
+            name: 'Invoices',
+            href: '/dashboard/invoices',
+        },
+        {
+            name: 'Customers',
+            href: '/dashboard/customers',
+        },
+    ];
+
+    return (
+        <div>
+            <ul>
+                {links.map((link) => (
+                    <li key={link.name}>
+                        <Link
+                            href={link.href}
+                            className={`hover:bg-slate-300 ${
+                                pathname === link.href
+                                    ? 'text-blue-300 bg-slate-50'
+                                    : ''
+                            }`}
+                        >
+                            {link.name}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
